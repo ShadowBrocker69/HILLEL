3ca7de5ebae707c2f1f765461bb26d924be9d718
AdamKadura <48388038+ShadowBrocker69@users.noreply.github.com>
2022-12-09 10:08:17 +0200

    Add files via upload

--- homework_8/ex_2.py
+++ homework_8/ex_2.py
@@ -1,11 +1,13 @@
 str = input("Введите предложение, в котором больше друх слов: ").split(" ")
-while len(str) < 3:
-    str = input("Требуется ввести больше двух слов: ").split(" ")
 
-str.sort()
-
-while len(str[0]) == 0:
-    str.pop(0)
+while True:
+    str.sort()
+    while len(str[0]) == 0:
+        str.pop(0)
+    if len(str) < 3:
+        str = input("Требуется ввести больше двух слов: ").split(" ")
+        continue
+    break
 
 ind = "Индексы:"
 noun = "Элементы списка:"
