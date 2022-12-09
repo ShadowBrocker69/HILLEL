4d64bb1ccba92438349773e8a98e26d61269b2ba
AdamKadura <48388038+ShadowBrocker69@users.noreply.github.com>
2022-12-09 10:14:30 +0200

    Add files via upload

--- homework_5/ex_3.py
+++ homework_5/ex_3.py
@@ -1,21 +1,4 @@
-# N = [int(n) for n in input('Введите значения цен через пробел ').split()]
-#
-# paired = 0
-# not_paired = 0
-#
-# for i in N:
-#     if i % 2 == 0:
-#         paired += 1
-#     if i % 2 > 0:
-#         not_paired += 1
-#
-# print("Сумма введённых чисел = ", sum(N))
-# print("Среднее арифметическое = ", sum(N)/len(N))
-# print("Максимальное значение = ", max(N))
-# print("Количество парных чисел = ", paired)
-# print("Количество непарных чисел = ", not_paired)
-
-N = int(input("Введите числовое значение "))
+N = 0
 S = 0
 С = 0
 paired = 0
@@ -27,7 +10,6 @@ while True:
     С += 1
     S += N
     N = int(input("Введите числовое значение "))
-
     if N >= Max:
         Max = N
         if Max == 0:
@@ -41,7 +23,6 @@ while True:
         not_paired += 1
     if N == 0:
         break
-
 
 if Max > 0:
     print("Сумма введённых чисел = ", S)
